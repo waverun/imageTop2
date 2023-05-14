8ed7273921f933cad6665a3d1f3159d111cbde2c
Shay Moreno <moa129@gmail.com>
2023-05-14 10:43:10 +0300

    Initial Commit

---
+++ imageTop2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  imageTop2
+//
+//  Created by shay moreno on 14/05/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
