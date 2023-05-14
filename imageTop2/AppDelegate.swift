67e11f8e21ee7ae6a638df3a730cb9a0053eed4f
Shay Moreno <moa129@gmail.com>
2023-05-14 15:34:05 +0300

    all windows

--- imageTop2/AppDelegate.swift
+++ imageTop2/AppDelegate.swift
@@ -4,39 +4,45 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window1: NSWindow!
-    var window2: NSWindow!
+    var windows: [NSWindow] = []
+//    var window2: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Grab the screens
-        let screens = NSScreen.screens
-        guard screens.count >= 2 else {
-            print("Less than two screens connected.")
-            return
-        }
+//        let screens = NSScreen.screens
+//        guard screens.count >= 2 else {
+//            print("Less than two screens connected.")
+//            return
+//        }
 
         // Create a SwiftUI view for each window
-        let contentView1 = ContentView().background(Color.red)
-        let contentView2 = ContentView().background(Color.blue)
+//        let contentView1 = ContentView().background(Color.red)
+//        let contentView2 = ContentView().background(Color.blue)
 
         // Create the first window
-        window1 = NSWindow(contentRect: screens[0].frame,
-                           styleMask: [.titled, .closable, .resizable, .fullSizeContentView],
-                           backing: .buffered, defer: false, screen: screens[0])
-        window1.center()
-        window1.setFrame(screens[0].frame, display: true)
-        window1.contentView = NSHostingView(rootView: contentView1)
-        window1.makeKeyAndOrderFront(nil)
-        window1.toggleFullScreen(nil) // Add this line
+        var i = -1
+        for screen in NSScreen.screens {
+            i += 1
+            let contentView = ContentView()
+            let window = NSWindow(contentRect: screen.frame,
+                               styleMask: [.titled, .closable, .resizable, .fullSizeContentView],
+                               backing: .buffered, defer: false, screen: screen)
+            window.center()
+            window.setFrame(screen.frame, display: true)
+            window.contentView = NSHostingView(rootView: contentView)
+            window.makeKeyAndOrderFront(nil)
+            window.toggleFullScreen(nil) // Add this line
 
-        // Create the second window
-        window2 = NSWindow(contentRect: screens[1].frame,
-                           styleMask: [.titled, .closable, .resizable, .fullSizeContentView],
-                           backing: .buffered, defer: false, screen: screens[1])
-        window2.center()
-        window2.setFrame(screens[1].frame, display: true)
-        window2.contentView = NSHostingView(rootView: contentView2)
-        window2.makeKeyAndOrderFront(nil)
-        window2.toggleFullScreen(nil) // Add this line
+            windows.append(window)
+            // Create the second window
+//            window2 = NSWindow(contentRect: screens[1].frame,
+//                               styleMask: [.titled, .closable, .resizable, .fullSizeContentView],
+//                               backing: .buffered, defer: false, screen: screens[1])
+//            window2.center()
+//            window2.setFrame(screens[1].frame, display: true)
+//            window2.contentView = NSHostingView(rootView: contentView2)
+//            window2.makeKeyAndOrderFront(nil)
+//            window2.toggleFullScreen(nil) // Add this line
+        }
     }
 }
