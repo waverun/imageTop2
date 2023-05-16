77b7d1e958b9dfa7aad032916053e4a82c06b79b
Shay Moreno <moa129@gmail.com>
2023-05-16 16:34:49 +0300

    change load image to prevent run time error on setting error

--- imageTop2/Structs/LoadableImageView.swift
+++ imageTop2/Structs/LoadableImageView.swift
@@ -1,27 +1,17 @@
 import SwiftUI
 
-struct LoadableImage: View { 
+struct LoadableImage: View {
     let imagePath: String
     let onError: () -> Void
 
+    @State private var nsImage: NSImage? = nil
     @State private var imageLoadError = false
-
-    private func loadImage() -> NSImage? {
-        if let nsImage = NSImage(contentsOfFile: imagePath) {
-            return nsImage
-        } else {
-            imageLoadError = true
-            return nil
-        }
-    }
 
     var body: some View {
         Group {
-            if let nsImage = loadImage(), !imageLoadError {
-                Image(nsImage: nsImage)
+            if let image = nsImage, !imageLoadError {
+                Image(nsImage: image)
                     .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                     .clipped()
                     .edgesIgnoringSafeArea(.all)
             } else {
@@ -31,5 +21,54 @@ struct LoadableImage: View {
                     }
             }
         }
+        .onAppear(perform: loadImage)
+    }
+
+    private func loadImage() {
+        DispatchQueue.global().async {
+            if let image = NSImage(contentsOfFile: self.imagePath) {
+                DispatchQueue.main.async {
+                    self.nsImage = image
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.imageLoadError = true
+                }
+            }
+        }
     }
 }
+
+//import SwiftUI
+//
+//struct LoadableImage: View {
+//    let imagePath: String
+//    let onError: () -> Void
+//
+//    @State private var imageLoadError = false
+//
+//    private func loadImage() -> NSImage? {
+//        if let nsImage = NSImage(contentsOfFile: imagePath) {
+//            return nsImage
+//        } else {
+//            imageLoadError = true
+//            return nil
+//        }
+//    }
+//
+//    var body: some View {
+//        Group {
+//            if let nsImage = loadImage(), !imageLoadError {
+//                Image(nsImage: nsImage)
+//                    .resizable()
+//                    .clipped()
+//                    .edgesIgnoringSafeArea(.all)
+//            } else {
+//                Color.clear
+//                    .onAppear {
+//                        onError()
+//                    }
+//            }
+//        }
+//    }
+//}
