9a9db5f232f714614ce0b83053ab7ca6a4860cdb
Shay Moreno <moa129@gmail.com>
2023-06-18 17:30:10 +0300

    change var to let

--- imageTop2/Functions/clearPexelVideos.swift
+++ imageTop2/Functions/clearPexelVideos.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 func clearPexelVideos(folderURL: URL, fileName: String) {
-    var fileURL = folderURL.appendingPathComponent(fileName)
+    let fileURL = folderURL.appendingPathComponent(fileName)
     deleteFile(at: fileURL)
 }
