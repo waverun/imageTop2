e5db5e53024087626b0e1d76e6d5e677e0d96c35
Shay Moreno <moa129@gmail.com>
2023-06-29 16:57:32 +0300

    correct fade videos

--- imageTop2/ContentView.swift
+++ imageTop2/ContentView.swift
@@ -133,7 +133,7 @@ struct ContentView: View {
                         loadRandomImage()
                     }
                     .opacity(showVideo && showSecondVideo ? 1 : 0)
-                    .animation(.linear(duration: showVideo && showSecondVideo ? 4 : 4), value: showVideo && !showSecondVideo)
+                    .animation(.linear(duration: showVideo && showSecondVideo ? 4 : 4), value: showVideo && showSecondVideo)
                     .edgesIgnoringSafeArea(.all)
                 }
 
