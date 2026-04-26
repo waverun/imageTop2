47b415d5f1cb085d9472c7cdba40f74434607453
Shay Moreno <moa129@gmail.com>
2026-04-26 05:03:37 +0300

    Settings

--- imageTop2/AppDelegate.swift
+++ imageTop2/AppDelegate.swift
@@ -351,8 +351,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject, NSWindowDe
 
         updateShowItem()
 
-        menu.addItem(withTitle: "Skip Current (Esc / Right Click)", action: #selector(skipCurrentItem), keyEquivalent: "\u{1b}")
         menu.addItem(withTitle: "Settings", action: #selector(openSettings), keyEquivalent: "")
+        menu.addItem(withTitle: "Skip Current (Esc / Right Click)", action: #selector(skipCurrentItem), keyEquivalent: "\u{1b}")
         autoStartItem = menu.addItem(withTitle: (autoStart ? "Disable" : "Enable") + " Auto (Inactivity) Start", action: #selector(handleAutoStart), keyEquivalent: "")
 
         menu.addItem(NSMenuItem.separator())
